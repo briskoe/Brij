a83df01d5a7331bd5f78f21d50531e5530ebf808
manu4321 <manu_4321@hotmail.com>
2016-09-23 07:52:14 -0400

    Add function that allows to get parameter from url
    This function will allow to take the value of a parameter passed in the URL

--- brij-client/www/js/index.js
+++ brij-client/www/js/index.js
@@ -40,6 +40,15 @@ $(function(){
     }
 });
 
+$.urlParam = function(name){
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (results==null){
+       return null;
+    }
+    else{
+       return results[1] || 0;
+    }
+}
 
 
 
