0464f688857d31e90bcf0d1d3b9b34adc3fa0bea
Capstone\Evan <evandaily@hotmail.ca>
2016-10-31 15:04:01 -0400

    update login page
    -remade login page
    -adjusted javascript and css

--- brij-client/www/js/login.js
+++ brij-client/www/js/login.js
@@ -3,20 +3,24 @@
  */
 
 $(function () {
+    $("#error").hide();
     checkIfOnline(checkIfOnlineCallback, false);
     $("#btnSignIn").click(function (e) {
         e.preventDefault();
         e.stopPropagation();
         var username = $("#loginForm #username").val();
         var password = $("#loginForm #password").val();
-        var url = LOGIN;
-        url = url.replace(":username", username).replace(":password", password);
-        makeRequest(url, POST, "", "", function (data) {
-            //if online go to the homePage as you have permission
-            checkIfOnline(checkIfOnlineCallback, false);
+        if (!(username === "" && password === "")) {
+            var url = LOGIN;
+            url = url.replace(":username", username).replace(":password", password);
+            makeRequest(url, POST, "", "", function (data) {
+                //if online go to the homePage as you have permission
+                checkIfOnline(checkIfOnlineCallback, false);
 
-        }, defaultError);
-
+            }, error);
+        } else {
+            error();
+        }
     });
 
     $("#btnRegistration").click(function (e) {
@@ -99,6 +103,8 @@ function completeAccount(when) {
         //if online go to the homePage as you have permission
         checkIfOnline(callbackLocation, false);
     }, defaultError);
+}
 
-
+function error(e) {
+    $("#error").show();
 }
